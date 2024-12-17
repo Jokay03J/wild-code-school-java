b67fbc8ec05e7248de51094cfb1776fbeb419bc5
jokay03J <magrojoan3@gmail.com>
2024-12-17 15:04:46 +0100

    reverte

--- Hangar.java
+++
@@ -1,9 +0,0 @@
-public class Hangar {
-
-    public static void main(String[] args) {
-        Car clio = new Car("Clio", 0);
-        Boat titanic = new Boat("Titanic", 0);
-        System.out.println(clio.doStuff());
-        System.out.println(titanic.doStuff());
-    }
-}
