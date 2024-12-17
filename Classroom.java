55cabcbc650e345471825a53e414b9fa2d1c4a06
jokay03J <magrojoan3@gmail.com>
2024-12-17 14:49:15 +0100

    Java #7

--- Classroom.java
+++
@@ -1,9 +0,0 @@
-class Classroom {
-
-    public static void main(String[] args) {
-        Wilder firstWilder = new Wilder("Jean-Claude", true);
-        Wilder secondWilder = new Wilder("Henri", false);
-        System.out.println(firstWilder.whoAmI());
-        System.out.println(secondWilder.whoAmI());
-    }
-}
